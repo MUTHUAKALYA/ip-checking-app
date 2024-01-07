802a887a05ebc4ac350f8d4d6032d02a363c9cdc
MuthuAkalyaA <muthuakalyaa@gmail.com>
2024-01-08 02:06:52 +0530

    design implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-// Write your javascript code here
+const ipAddressPlaceholder=document.querySelector("#ipaddress")
+// get the ip address from this url "https://ipv4.icanhazip.com/"
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange=function(){
+    if(this.readyState=== 4 && this.status === 200){
+        console.log(xhr.responseText)
+        ipAddressPlaceholder.textContent = xhr.responseText;
+    }
+    else{
+        ipAddressPlaceholder.textContent= "IP address not found"
+    }
+};
+xhr.open("GET","https://ipv4.icanhazip.com/")
+xhr.send();
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
+  base: '/ip-checking-app/'
   //   build: {
   //     rollupOptions: {
   //       input: {
